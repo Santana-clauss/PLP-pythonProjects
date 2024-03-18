9d7a5a7f9ba92fc9fc54b998ea3f009f15a48e48
santana clauss <jepchumbasantana@gmail.com>
2024-03-18 09:27:16 +0300

    wk3 assignment

---
+++ CourseModules/wk2assignments/largepower.py
@@ -0,0 +1,18 @@
+def large_power(base,exp):
+    #print(num)
+    
+    if base**exp>5000:
+        print("The result is true")
+    else:
+        print("The result is false")
+print(large_power(10,2))
+
+def divisible_ten(num):
+    if num%10==0:
+        print("The number is divisible by 10")
+    else:
+        print("The number is not divisible by 10")
+        #return False
+divisible_ten(34)
+
+    
